a929ad1d7ab81dcf5d6113fb4a77c6f5213550fe
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-07-06 17:54:38 -0400

    created SpecialFilters component

---
+++ src/schemas/SpecialFilterSchema.ts
@@ -0,0 +1,16 @@
+import yup from 'src/@core/utils/customized-yup'
+
+const location = yup.object().shape({
+  id: yup.number(),
+  name: yup.string()
+}).nullable()
+
+const specialFilterSchema = yup.object({
+  city: location,
+  zone: yup.string(),
+  status: yup.string(),
+  operation: yup.string(),
+  category: yup.string(),
+})
+
+export default specialFilterSchema
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,6 +2,7 @@ export { default as AddressSchema } from './AddressSchema';
 export * from './BankSchema';
 export { default as EmailSchema } from './EmailSchema';
 export { default as EmployeeSchema } from './EmployeeSchema';
+export { default as SpecialFilterSchema } from './SpecialFilterSchema';
 export { default as adminBillingSchema } from './adminBillingSchema';
 export * from './updateCredentialsSchema';
 export { default as userSchema } from './userSchema';
