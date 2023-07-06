ecb1660b7980da059124fe7c409cd1e973f81e28
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-07-06 17:53:10 -0400

    create ListingTable component

--- src/components/Tables/listing/components/ListingTableRow.tsx
+++ src/components/Tables/listing/components/ListingTableRow.tsx
@@ -10,59 +10,59 @@ import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
-import { useTranslation } from 'react-i18next'
-
-
 
 import { GridRowProps, GridRow } from '@mui/x-data-grid'
 
-const ListingTableRow = () => {
-  // const { t } = useTranslation()
-  // const [ opened, setOpen ] = useState(false)
+const ListingTableRow = ({gridRowProps}: ListingRowProps) => {
+  const [ opened, setOpen ] = useState(false)
 
-  // const {commercialName, phones, email, tags}: TAccountsRow = gridRowProps.row as any
+  const {bathrooms, rooms, adSite, adDate, adOwner, userAd} = gridRowProps.row as any
 
-  // return (
-  //   <>
-  //     <Stack direction={"row"} alignItems="center" justifyContent="center"
-  //       borderBottom={({palette})=> `${palette.divider} solid 1px`}
-  //       sx={({palette}) => ({"&:hover": {backgroundColor: palette.customColors.tableHeaderBg}})}
-  //     >
-  //       <Box px={"10px"}>
-  //         <IconButton aria-label='expand row' size='small' onClick={() => setOpen(!opened)}>
-  //             <Icon icon={`tabler:${opened ? 'chevron-up' : 'chevron-down'}`} width={20}/>
-  //         </IconButton>
-  //       </Box>
-  //       <GridRow {...gridRowProps}/>
-  //     </Stack>
-  //     <Collapse in={opened} timeout='auto'>
-  //       <Box sx={{ margin: 2, width: "100%" }}>
-  //         <Table size='small' aria-label='purchases'>
-  //           <TableHead>
-  //             <TableRow>
-  //               <TableCell align='left'>{t('commercial-name_one')}</TableCell>
-  //               <TableCell align='left'>{t('phone_one')}</TableCell>
-  //               <TableCell align='left'>{t('email')}</TableCell>
-  //               <TableCell align='left'>Tags</TableCell>
-  //             </TableRow>
-  //           </TableHead>
-  //           <TableBody>
-  //             <TableRow>
-  //               <TableCell align='left'>{commercialName}</TableCell>
-  //               <TableCell align='left'>{phones.join(" / ")}</TableCell>
-  //               <TableCell align='left'>{email}</TableCell>
-  //               <TableCell align='left'>{tags}</TableCell>
-  //             </TableRow>
-  //           </TableBody>
-  //         </Table>
-  //       </Box>
-  //     </Collapse>
-  //   </>
-  // )
+  return (
+    <>
+      <Stack direction={"row"} alignItems="center" justifyContent="center"
+        borderBottom={({palette})=> `${palette.divider} solid 1px`}
+        sx={({palette}) => ({"&:hover": {backgroundColor: palette.customColors.tableHeaderBg}})}
+      >
+        <Box px={"10px"}>
+          <IconButton aria-label='expand row' size='small' onClick={() => setOpen(!opened)}>
+              <Icon icon={`tabler:${opened ? 'chevron-up' : 'chevron-down'}`} width={20}/>
+          </IconButton>
+        </Box>
+        <GridRow {...gridRowProps}/>
+      </Stack>
+      <Collapse in={opened} timeout='auto'>
+        <Box sx={{ margin: 2, width: "100%" }}>
+          <Table size='small' aria-label='purchases'>
+            <TableHead>
+              <TableRow>
+                <TableCell align='left'>Baños</TableCell>
+                <TableCell align='left'>Habitaciones</TableCell>
+                <TableCell align='left'>Publicado en</TableCell>
+                <TableCell align='left'>Fecha Anuncio</TableCell>
+                <TableCell align='left'>Nombre Anunciante</TableCell>
+                <TableCell align='left'>Usuario Anuncio</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell align='left'>{bathrooms}</TableCell>
+                <TableCell align='left'>{rooms}</TableCell>
+                <TableCell align='left'>{adSite}</TableCell>
+                <TableCell align='left'>{adDate}</TableCell>
+                <TableCell align='left'>{adOwner}</TableCell>
+                <TableCell align='left'>{userAd}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Box>
+      </Collapse>
+    </>
+  )
 }
 
-// type AccountsRowProps = {
-//   gridRowProps: GridRowProps,
-// }
+type ListingRowProps = {
+  gridRowProps: GridRowProps,
+}
 
 export default ListingTableRow
