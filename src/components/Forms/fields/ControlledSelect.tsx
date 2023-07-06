93bf30af52f1bfbb312d55e47358c4e8b69b7ecc
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-07-06 17:55:49 -0400

    Controlled select accepts options prop

--- src/components/Forms/fields/ControlledSelect.tsx
+++ src/components/Forms/fields/ControlledSelect.tsx
@@ -2,20 +2,26 @@ import { memo } from 'react'
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import Select, { type SelectProps } from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
 
 import { useFormContext, Controller } from 'react-hook-form'
 
 type ControlledSelectProps = {
   name: string
   label: string
+  options?: string[]
 } & Exclude<SelectProps, 'name' | 'label'>
 
 const ControlledSelect = (props: ControlledSelectProps) => {
-  const { name, label, children } = props
+  const { name, label, options = [] ,children } = props
   const {
     control,
     formState: { errors }
   } = useFormContext()
+
+  const menuItems = options.map(option =>
+    <MenuItem key={option} value={option}>{option}</MenuItem>
+  )
 
   return (
     <Controller
@@ -37,6 +43,7 @@ const ControlledSelect = (props: ControlledSelectProps) => {
             labelId='related-account-label'
           >
             {children}
+            {menuItems}
           </Select>
         </FormControl>
       )}
