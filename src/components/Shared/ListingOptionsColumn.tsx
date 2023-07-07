4a033cacaf34d81b27f88c43fe677776f5f8de75
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-07-07 00:21:46 -0400

    created reusable component for ListingTable

---
+++ src/components/Shared/ListingOptionsColumn.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+
+import { type GridRenderCellParams } from "@mui/x-data-grid";
+
+import { Counter, ActionButton } from "src/components/Shared";
+
+type Props = {
+  openEmailModal: (id: string) => void
+} & GridRenderCellParams
+
+const ListingOptionColumn = (props: Props) => {
+  const {id, row: { comments, link, count, email }} = props
+
+  return (
+    <Stack
+      justifyContent="start"
+      direction="row"
+      alignItems="center"
+      gap={0.25}
+    >
+      <a href={link}>
+        <ActionButton
+          title={"Ver"}
+          icon="tabler:eye"
+          buttonProps={{ sx: { padding: "4px" } }}
+        />
+      </a>
+      <ActionButton
+        title={"Enviar email"}
+        icon="tabler:mail"
+        buttonProps={{
+          onClick: () => {
+            props.openEmailModal(email);
+          },
+          sx: { padding: "4px" },
+        }}
+      />
+      <Box padding={"4px"}>
+        <Counter value={count} />
+      </Box>
+      <ActionButton
+        title={"Adjuntar"}
+        icon="tabler:paperclip"
+        buttonProps={{
+          component: Link,
+          href: `/documents/${id}`,
+          passHref: true,
+          sx: { padding: "4px" },
+        }}
+      />
+      <ActionButton
+        title="Info"
+        icon="tabler:info-circle-filled"
+        buttonProps={{ sx: { padding: "4px" } }}
+      />
+      {/* <ActionButton title='Comentarios'
+          icon={`majesticons:${comments.length === 0 ? 'comment-text-line' : 'comment-text'}`}
+          buttonProps={{onClick: () => {openCommentsModal(comments)('id') }}}
+        /> */}
+    </Stack>
+  );
+};
+
+export default ListingOptionColumn
