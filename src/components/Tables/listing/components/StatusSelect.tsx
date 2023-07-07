84e8bdc2acb639d65ea19c1eaaecdf5b1ccf16d5
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-07-06 20:09:21 -0400

    Status select uses real values

--- src/components/Tables/listing/components/StatusSelect.tsx
+++ src/components/Tables/listing/components/StatusSelect.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
 
+import { STATUSES } from 'src/types'
+
 type StatusSelectProps = {
   value: string
 }
@@ -12,8 +14,9 @@ export const StatusSelect = ({value}: StatusSelectProps) => {
 
   return (
     <Select fullWidth size="small" value={status} onChange={(e) => setStatus(e.target.value)}>
-        <MenuItem value='contact'>Contacto</MenuItem>
-        <MenuItem value='e'>ejemplo</MenuItem>
+        {STATUSES.map(status => (
+          <MenuItem key={status} value={status}>{status}</MenuItem>
+        ))}
     </Select>
   )
 }
