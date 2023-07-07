360b2d753b84f8f8f0924a8bb781c34c653cab9f
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-07-07 00:22:54 -0400

    created GridColDef for listing history table

---
+++ src/components/Tables/listing/ColumnDefs/listingHistoryColumns.tsx
@@ -0,0 +1,78 @@
+import { GridColDef} from '@mui/x-data-grid'
+
+import { StatusSelect } from '../components'
+import ListingOptionColumn from 'src/components/Shared/ListingOptionsColumn'
+
+type optionActions = {
+  // openDeleteModal: (id: string) => void
+  // openCommentsModal: (comments: string) => (id: string) => void
+  openEmailModal: (email: string) => void
+}
+
+const listingColumns = ({openEmailModal}: optionActions): GridColDef[] => {
+  return [
+    {
+      field: 'operation',
+      headerName: 'Operacion',
+      filterable: false,
+      width: 120,
+    },
+    {
+      field: 'category',
+      headerName: 'Categoría',
+      filterable: false,
+      width: 100,
+    },
+    {
+      field: 'title',
+      headerName: 'Título',
+      filterable: false,
+      width: 300
+    },
+    {
+      field: 'price',
+      headerName: 'Precio',
+      filterable: false,
+      width: 80
+    },
+    {
+      field: 'meters',
+      headerName: 'Metros',
+      filterable: false,
+      width: 100
+    },
+    {
+      field: 'user',
+      headerName: 'Usuario',
+      filterable: false,
+      width: 120,
+    },
+    {
+      field: 'timestamp',
+      headerName: 'Hora',
+      filterable: false,
+      valueFormatter: ({value}: {value: Date}) => {
+        return value.toLocaleTimeString('es-ES', {hour12: true, hour: "numeric", minute: "numeric"})
+      },
+      width: 100,
+    },
+    {
+      field: 'status',
+      headerName: 'Estado',
+      filterable: false,
+      width: 140,
+      renderCell: ({row: { status }}) => <StatusSelect value={status}/>
+    },
+    {
+      field: 'options',
+      headerName: 'Opciones',
+      width: 260,
+      filterable: false,
+      sortable: false,
+      renderCell: (row) =>
+        <ListingOptionColumn {...row} openEmailModal={openEmailModal}/>
+    }
+  ]
+}
+
+export default listingColumns
