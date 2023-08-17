8eb576426b568661b9d9699db87a667e3bc17cec
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-08-17 17:53:02 -0400

    update login service and types

--- src/types/Auth.d.ts
+++ src/types/Auth.d.ts
@@ -6,67 +6,9 @@ export type LoginParams = {
   rememberMe?: boolean
 }
 
-// export type RegisterParams = {
-//   email: string
-//   username: string
-//   password: string
-// }
-
-export type RegisterParams = {
-  tenant_data: {
-    city: string
-    email: string
-    nif: string
-    phone: string
-    postcard: string
-    province: string
-    street: string
-    tenant_name: string
-    bank_name: string
-    banck_account: string
-  }
-  admin_data: {
-    city: string
-    coin: string
-    country: string
-    direction: string
-    dni: string
-    email: string
-    id: string
-    language: string
-    lastname: string
-    password: string
-    phone: string
-    postcard: string
-    province: string
-    username: string
-    user_state: string
-  }
-}
-
-export type UserDataType = {
-  ID: number
-  CreatedAt: string
-  UpdatedAt: string
-  firstName: string
-  lastName: string
-  email: string
-  contact: string
-}
-
-
 export type User = {
-  admin: boolean,
-  email: string
-}
-
-
-
-export type JWTStructure = {
-  exp: 1682786714
-  id: string
-  name: string
-  tenantId: string
+  fullname: string
+  is_admin: boolean
 }
 
 export type AuthValuesType = {
@@ -74,7 +16,6 @@ export type AuthValuesType = {
   user: User | null
   setLoading: (value: boolean) => void
   login: (params: LoginParams, errorCallback?: ErrCallbackType) => void
-  adminLogin: (params: LoginParams, errorCallback?: ErrCallbackType) => void,
   logout: (redirectToLogin?: boolean) => void
   register: (params: RegisterParams, errorCallback?: ErrCallbackType) => void
 }
