56d1157ec8f9cdb17d6cef3d9820ea8c19cb31b7
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-08-18 17:20:27 -0400

    add get users from backend

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,10 +1,42 @@
+import { useState } from 'react'
 
-import { UserTable } from 'src/components/Tables'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+
+import { useQuery } from '@tanstack/react-query'
+
+import { UserTable, TableWrapper } from 'src/components/Tables'
+
+import { getAll } from 'src/services/users'
+
+import { useDebouncedState } from 'src/hooks'
 
 const TableList = () => {
+  const [page, setPage] = useState(0)
+  const [search, debouncedSearch, setSearch] = useDebouncedState('')
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['get-users'],
+    queryFn: () => getAll(page, debouncedSearch),
+    keepPreviousData: true,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false
+  })
 
   return (
-      <UserTable />
+    <Card>
+      <CardContent>
+        <TableWrapper
+          page={page}
+          setPage={setPage}
+          loading={isLoading || search != debouncedSearch}
+          error={isError}
+          rows={data || []}
+        >
+          <UserTable rows={data || []} search={search} setSearch={setSearch} />
+        </TableWrapper>
+      </CardContent>
+    </Card>
   )
 }
 
