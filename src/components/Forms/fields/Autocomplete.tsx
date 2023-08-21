47d44cffa58f490ffb01baf4c8a68fc97c45b13d
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-08-21 17:32:02 -0400

    add reusable controlled Autocomplete component

---
+++ src/components/Forms/fields/Autocomplete.tsx
@@ -0,0 +1,58 @@
+import TextField from '@mui/material/TextField'
+import FormControl from '@mui/material/FormControl'
+import MUIAutocomplete from '@mui/material/Autocomplete'
+
+import { useFormContext, Controller } from 'react-hook-form'
+
+type AutocompleteProps = {
+  options: unknown[]
+  name: string
+  label: string
+  loading: boolean
+  error: boolean
+  isOptionEqualToValue: (option: any, value: any) => any
+}
+
+const Autocomplete = ({ name, label, loading, error, options, isOptionEqualToValue }: AutocompleteProps) => {
+  const {
+    control,
+    formState: { errors }
+  } = useFormContext()
+
+  return (
+    <FormControl fullWidth>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { onChange, value, ...rest } }) => (
+          <MUIAutocomplete
+            {...rest}
+            loading={loading}
+            value={value}
+            options={options || []}
+            onChange={(event, newValue) => {
+              onChange(newValue || null)
+            }}
+            getOptionLabel={option => option.name || ''}
+            isOptionEqualToValue={isOptionEqualToValue}
+            noOptionsText={error ? 'Error de busqueda, intente de nuevo' : 'Sin resultados'}
+            renderInput={params => (
+              <TextField
+                {...params}
+                label={label}
+                inputProps={{
+                  ...params.inputProps,
+                  autoComplete: 'new-password' // disable autocomplete and autofill
+                }}
+                error={Boolean(errors[name])}
+                helperText={errors[name] && 'Selecciona una poblacion'}
+              />
+            )}
+          />
+        )}
+      />
+    </FormControl>
+  )
+}
+
+export default Autocomplete
