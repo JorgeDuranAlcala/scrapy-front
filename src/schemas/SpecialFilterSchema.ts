80c0fadcc96a45829266e9d1b231a46bc8f1c5db
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-08-21 17:32:56 -0400

    update SpeciaFilterSchema

--- src/schemas/SpecialFilterSchema.ts
+++ src/schemas/SpecialFilterSchema.ts
@@ -3,10 +3,11 @@ import yup from 'src/@core/utils/customized-yup'
 const location = yup.object().shape({
   id: yup.number(),
   name: yup.string()
-}).nullable()
+}).nullable().default(null)
 
 const specialFilterSchema = yup.object({
-  city: location,
+  province: location,
+  municipality: yup.object({ province_id: yup.number() }).concat(location),
   zone: yup.string(),
   status: yup.string(),
   operation: yup.string(),
