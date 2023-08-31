27b6a8191485b4be7ee7815a2130d152d0e35da0
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-08-31 19:00:59 -0400

    changed state logic

--- src/components/Shared/Counter.tsx
+++ src/components/Shared/Counter.tsx
@@ -1,62 +1,54 @@
-import { useState, memo, useEffect} from 'react'
+import { memo } from 'react'
 
-import Badge from "@mui/material/Badge"
-import Button from "@mui/material/Button"
-import ButtonGroup from "@mui/material/ButtonGroup"
+import Badge from '@mui/material/Badge'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 
 import Icon from 'src/@core/components/icon'
+import { useDebouncedState } from 'src/hooks'
 
 type CounterProps = {
   value: number
 }
 
-export const Counter = memo(({value}: CounterProps) => {
-  const [count, setCount] = useState<number>(value)
-  const [savedCount, setSavedCount] =useState(value)
+export const Counter = memo(({ value }: CounterProps) => {
+  const [count, debouncedCount, setCount] = useDebouncedState(0)
 
-
-  const submitTimeout = setTimeout(() => {
-    if(count !== savedCount){
-      console.log('submit data', count)
-      setSavedCount(count)
-    }
-  }, 1000)
-
-  useEffect(() => {
-    clearTimeout(submitTimeout)
-    return () => { clearTimeout(submitTimeout)}
-  },[])
+  console.log(count, debouncedCount)
 
   const handleCount = (option: string) => {
-    const number = option == 'add'
-      ? Math.min(count + 1, 9)
-      : Math.max(count - 1, 0)
+    const number = option == 'add' ? 1 : -1
 
-    setCount(number)
-
-    // if(number !== count) clearTimeout(submitTimeout)
+    if (value + count + number <= 9 && value + count + number >= 0)
+      setCount(count + number)
   }
 
   return (
-    <ButtonGroup size='small' sx={{maxHeight: "20px"}}>
-      <Badge badgeContent={count} color='primary' max={9}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}>
-        <Button aria-label='reduce'
-          sx={{borderTopRightRadius: 0, borderBottomRightRadius: 0}}
+    <ButtonGroup size='small' sx={{ maxHeight: '20px' }}>
+      <Badge
+        badgeContent={count + value}
+        color='primary'
+        max={9}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+      >
+        <Button
+          aria-label='reduce'
+          sx={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
           onClick={() => handleCount('minus')}
         >
           <Icon icon='tabler:minus' fontSize={15} />
         </Button>
       </Badge>
-      <Button aria-label='increase'
-          sx={{borderTopLeftRadius: 0, borderBottomLeftRadius: 0}}
-          onClick={() => handleCount('add')}
-        >
+      <Button
+        aria-label='increase'
+        sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+        onClick={() => handleCount('add')}
+      >
         <Icon icon='tabler:plus' fontSize={15} />
       </Button>
-  </ButtonGroup>
+    </ButtonGroup>
   )
 })
