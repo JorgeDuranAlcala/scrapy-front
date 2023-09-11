bc93537ff6aab68e72fc72a2034e699215dcb8ce
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-09-11 18:51:53 -0400

    title, price, and phone cells are editable

--- src/components/Shared/EditableColumn.tsx
+++ src/components/Shared/EditableColumn.tsx
@@ -1,38 +1,48 @@
 import { useState, useEffect, ChangeEvent } from "react"
 import { GridRenderEditCellParams } from '@mui/x-data-grid'
-import OutlinedInput from "@mui/material/OutlinedInput"
+import TextField from "@mui/material/TextField"
 import CircularProgress from '@mui/material/CircularProgress';
 import InputAdornment from '@mui/material/InputAdornment';
+import Tooltip from "@mui/material/Tooltip";
+
+import Icon from 'src/@core/components/icon'
+
+type EditableColumnProps = {
+  msg?: string
+} & GridRenderEditCellParams
 
 // Custom edit component for DataGrid
-export const EditableColumn = ({value, id, api, field}: GridRenderEditCellParams) => {
+export const EditableColumn = ({value, id, api, field, error, msg = ''}: EditableColumnProps) => {
   const [currentValue, setCurrentValue] = useState(value)
 
   const handleValueChange = (event: ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value
     setCurrentValue(newValue)
-    api.setEditCellValue({id, field, value: newValue, debounceMs: 2000})
+    api.setEditCellValue({id, field, value: newValue, debounceMs: 1000})
   }
 
   useEffect(() => {
     // Save changes when no changes have been made in 2 seconds
-    if(value === currentValue)
+    if(value == currentValue)
       console.log("submit value: ", currentValue)
   }, [value])
 
   return (
-      <OutlinedInput
+      <TextField
         autoFocus
         fullWidth
         size="small"
         value={currentValue}
         onChange={handleValueChange}
-        endAdornment={ value !== currentValue
+        error={error}
+        helperText={error && <Tooltip title={msg}><Icon icon="tabler:alert-circle"/></Tooltip>}
+        InputProps={{
+          endAdornment: value != currentValue && !error
           ? <InputAdornment position="end">
-              <CircularProgress size={20}/>
+              <CircularProgress size={10}/>
             </InputAdornment>
           : undefined
-        }
+        }}
       />
   )
 }
--- src/components/Tables/listing/ColumnDefs/listingColumns.tsx
+++ src/components/Tables/listing/ColumnDefs/listingColumns.tsx
@@ -2,12 +2,15 @@ import { GridColDef} from '@mui/x-data-grid'
 
 import { StatusSelect } from '../components'
 import listingOptionColumn from 'src/components/Shared/ListingOptionsColumn'
+import { EditableColumn } from 'src/components/Shared'
 
 type optionActions = {
   openCommentsModal: (id: string, comments: string) => void
   openEmailModal: (email: string) => void
   route: string
 }
+
+const NAN_VALIDATION = 'El valor debe ser un numero'
 
 const listingColumns = ({openEmailModal, openCommentsModal, route}: optionActions): GridColDef[] => {
   return [
@@ -27,30 +30,47 @@ const listingColumns = ({openEmailModal, openCommentsModal, route}: optionAction
       field: 'title',
       headerName: 'Título',
       filterable: false,
-      width: 300
+      editable: true,
+      renderEditCell: (props) => <EditableColumn  {...props}/>,
+      width: 280
     },
     {
       field: 'price',
+      type: 'number',
       headerName: 'Precio',
       filterable: false,
-      width: 80
-    },
-    {
-      field: 'meters',
-      headerName: 'Metros',
-      filterable: false,
+      editable: true,
+      renderEditCell: (props) => <EditableColumn msg={NAN_VALIDATION} {...props}/>,
+      preProcessEditCellProps: (params) => ({ ...params.props, error: Number.isNaN(params.props.value)}),
       width: 100
     },
     {
-      field: 'sqrMtrPrice',
-      headerName: 'Precio MT2',
+      field: 'meters',
+      type: 'number',
+      headerName: 'Metros 2',
       filterable: false,
+      editable: true,
+      renderEditCell: (props) => <EditableColumn msg={NAN_VALIDATION} {...props}/>,
+      preProcessEditCellProps: (params) => ({ ...params.props, error: Number.isNaN(params.props.value)}),
+      width: 100,
+    },
+    {
+      field: 'sqrMtrPrice',
+      headerName: 'Precio M2',
+      filterable: false,
+      valueGetter: ({row: { sqrMtrPrice, price}}) => {
+        return price * sqrMtrPrice
+      },
       width: 120,
     },
     {
       field: 'phone',
+      type: 'number',
       headerName: 'Teléfono',
       filterable: false,
+      editable: true,
+      // preProcessEditCellProps: (params) => ({}),
+      renderEditCell: (props) => <EditableColumn {...props}/>,
       width: 100,
     },
     {
@@ -63,17 +83,10 @@ const listingColumns = ({openEmailModal, openCommentsModal, route}: optionAction
       renderEditCell: (props) => <StatusSelect {...props}/>,
     },
     {
-      field: 'email',
-      headerName: 'Email',
-      editable: true,
-      sortable: false,
-      disableColumnMenu: true,
-      hideable: false,
-    },
-    {
       type: 'actions',
       field: 'options',
       headerName: 'Opciones',
+      headerAlign: 'left',
       width: 260,
       getActions: (row) => listingOptionColumn({...row, listingSite: route, openEmailModal, openCommentsModal})
     }
