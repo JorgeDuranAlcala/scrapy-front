f35c66415bf4a5b01aa352d1c7266a9b7abda181
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-09-19 18:49:01 -0400

    fix build errors

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,4 +1,4 @@
 export { default as create } from './create';
+export { default as erase } from './erase';
 export { default as getAll } from './getAll';
-export { default as remove } from './remove';
 export { default as update } from './update';
