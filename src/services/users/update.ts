87d1d73a212d3d6a895c13d5b01beccde13e261a
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-09-23 15:36:13 -0400

    endpoint allows changing password

--- src/services/users/update.ts
+++ src/services/users/update.ts
@@ -1,9 +1,7 @@
 import { restRequestAuth } from 'src/services'
 import { type UserFormData } from 'src/components/Forms'
 
-const update = async ({id, password,...body}: UserFormData) => {
-  //TODO: Wait for decision on password update by admin
-
+const update = async ({id, ...body}: UserFormData) => {
   const response = await restRequestAuth('PUT', '/users/update', {
     body: { user_id: id, ...body}
   })
