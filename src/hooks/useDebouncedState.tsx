4889e8a89ed61c3f7ea5cbeb6bbfe01da1e8e898
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-09-23 22:58:10 -0400

    useDebounced state was not working correctly

--- src/hooks/useDebouncedState.tsx
+++ src/hooks/useDebouncedState.tsx
@@ -1,28 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 const DEFAULT_TIMEOUT = 1000 // Miliseconds
 
 // State management for situations requiring
 // setting state under a timeout constraint.
 // For example: A live-result searchbox
-const useDebouncedState = <T,>(initialState: T, timeout?: number) => {
+const useDebouncedState = <T,>(initialState: T, timeout= DEFAULT_TIMEOUT) => {
   const [debounced, setDebounce] = useState(initialState)
   const [data, setData] = useState(initialState)
 
-  const stateChange = setTimeout(() => {
-    if(debounced !== data){
-      setDebounce(data)
-    }
-  }, timeout || DEFAULT_TIMEOUT)
+  const stateChange = useMemo(()=> setTimeout(() => {
+    if(debounced !== data) setDebounce(data)
+  }, timeout || DEFAULT_TIMEOUT), [data])
 
   const handleChange = (input: typeof initialState) => {
     clearTimeout(stateChange)
     setData(input)
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     clearTimeout(stateChange)
-  }, [])
+    return () => clearTimeout(stateChange)
+  }, [initialState, timeout])
 
   return [data, debounced, handleChange] as const
 }
