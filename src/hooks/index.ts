9ece7f452fb7d98d1dacd6c5ac6163324cc414a7
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-10-03 09:55:52 -0400

    remove unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,3 @@ export { default as useAuth } from './useAuth';
 export { default as useDebouncedState } from './useDebouncedState';
 export { default as useDisclosure } from './useDisclosure';
 export { default as useFileRemove } from './useFileRemove';
-export { default as useProtectedAction } from './useProtectedAction';
-export { default as useTablePagination } from './useTablePagination';
