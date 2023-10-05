117b4100ea5e5bfe395857a9e2cca37911bee96d
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-10-05 00:26:40 -0400

    update barrel

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,4 @@
 export { default as EmailSchema } from './EmailSchema';
+export { default as MailerSchema } from './MailerSchema';
 export { default as SpecialFilterSchema } from './SpecialFilterSchema';
 export { default as userSchema } from './userSchema';
