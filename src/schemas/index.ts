e885e3f3b238b7e64408cb55f53b3fcd862b4c7d
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-10-04 23:29:48 -0400

    remove unused schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,8 +1,3 @@
-export { default as AddressSchema } from './AddressSchema';
-export * from './BankSchema';
 export { default as EmailSchema } from './EmailSchema';
-export { default as EmployeeSchema } from './EmployeeSchema';
 export { default as SpecialFilterSchema } from './SpecialFilterSchema';
-export { default as adminBillingSchema } from './adminBillingSchema';
-export * from './updateCredentialsSchema';
 export { default as userSchema } from './userSchema';
