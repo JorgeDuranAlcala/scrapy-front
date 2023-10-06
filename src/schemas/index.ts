90beee00327913118226d9b1777b9dd8ec4e106b
gianicola <gianicolajara@gmail.com>
2023-10-06 16:57:05 -0400

    exporting type from emailschema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,4 +1,4 @@
-export { default as EmailSchema } from './EmailSchema'
+export { default as EmailSchema, type EmailData } from './EmailSchema'
 export { default as MailerSchema } from './MailerSchema'
 export { default as SpecialFilterSchema } from './SpecialFilterSchema'
 export { default as userSchema } from './userSchema'
