7ed2e53df72685ae35ac9960a263484074a09f85
gianicola <gianicolajara@gmail.com>
2023-10-06 14:47:17 -0400

    fix errors in putConfigureEmails

--- src/services/notifications/putConfigureEmail.ts
+++ src/services/notifications/putConfigureEmail.ts
@@ -4,11 +4,11 @@ import { restRequestAuth } from '../rest-requests'
 const putConfigureEmail = async ({ email, password, securityProtocol, smtpHost, smtpPort }: MailerData) => {
   const response: Promise<{ status: number }> = await restRequestAuth('PUT', '/notify/configure', {
     body: {
-      mail_adress: email,
+      mail_address: email,
       mail_password: password,
       mail_server: smtpHost,
       mail_port: smtpPort,
-      mail_protocol: securityProtocol
+      mail_protocol: securityProtocol.toLowerCase()
     }
   })
 
