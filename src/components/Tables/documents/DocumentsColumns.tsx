223fc6347d4d9998bca5c6a85ab3b6ac383c05d4
gianicola <gianicolajara@gmail.com>
2023-10-09 18:03:59 -0400

    agregando checknull al valueFormatter de createdAt ya que la base de datos no devuelve este campo y puede dar error

--- src/components/Tables/documents/DocumentsColumns.tsx
+++ src/components/Tables/documents/DocumentsColumns.tsx
@@ -1,8 +1,8 @@
-import Box from "@mui/material/Box"
+import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import MuiLink from '@mui/material/Link'
-import Tooltip from "@mui/material/Tooltip"
-import { GridColDef} from '@mui/x-data-grid'
+import Tooltip from '@mui/material/Tooltip'
+import { GridColDef } from '@mui/x-data-grid'
 
 import { useTranslation } from 'react-i18next'
 
@@ -10,10 +10,10 @@ import { ActionButton, EditableColumn } from 'src/components/Shared'
 
 type optionActions = {
   openMailModal: (rowID: string) => void
-  deleteFile:(id: string) => void
+  deleteFile: (id: string) => void
 }
 
-const DocumentsColumns = ({openMailModal, deleteFile}: optionActions): GridColDef[] => {
+const DocumentsColumns = ({ openMailModal, deleteFile }: optionActions): GridColDef[] => {
   const { t, i18n } = useTranslation()
 
   return [
@@ -23,7 +23,7 @@ const DocumentsColumns = ({openMailModal, deleteFile}: optionActions): GridColDe
       filterable: false,
       sortable: false,
       width: 250,
-      renderCell: ({row: {name, viewLink}}) => (
+      renderCell: ({ row: { name, viewLink } }) => (
         <MuiLink component='a' href={viewLink} target='blank'>
           {name}
         </MuiLink>
@@ -36,13 +36,14 @@ const DocumentsColumns = ({openMailModal, deleteFile}: optionActions): GridColDe
       filterable: false,
       editable: true,
       width: 480,
-      renderCell: ({row: {description}}) =>
-        <Tooltip placement="top" title={t('document-description-edit')}>
-          <Box display="flex" minWidth="100%" minHeight="100%" alignItems="center">
+      renderCell: ({ row: { description } }) => (
+        <Tooltip placement='top' title={t('document-description-edit')}>
+          <Box display='flex' minWidth='100%' minHeight='100%' alignItems='center'>
             {description}
           </Box>
-        </Tooltip>,
-      renderEditCell: (params) => <EditableColumn {...params}/>
+        </Tooltip>
+      ),
+      renderEditCell: params => <EditableColumn {...params} />
     },
     {
       field: 'uploadedBy',
@@ -56,8 +57,8 @@ const DocumentsColumns = ({openMailModal, deleteFile}: optionActions): GridColDe
       headerName: t('upload-date') as string,
       sortable: false,
       filterable: false,
-      valueFormatter: ({value}) => value.slice(0,10),
-      width: 180,
+      valueFormatter: ({ value }) => value?.slice(0, 10),
+      width: 180
     },
     {
       field: 'options',
@@ -65,18 +66,29 @@ const DocumentsColumns = ({openMailModal, deleteFile}: optionActions): GridColDe
       width: 200,
       filterable: false,
       sortable: false,
-      renderCell: ({id, row: {downloadLink}}) =>
-      <Stack justifyContent='start' direction='row' spacing={0.25}>
-        <ActionButton title={'Descargar'} icon='tabler:download'
-          buttonProps={{href: downloadLink, download: true}}
-        />
-        <ActionButton  title='Eliminar' icon='tabler:trash'
-          buttonProps={{ onClick: () => deleteFile(id as string) }}
-        />
-        <ActionButton title='Enviar email' icon='tabler:mail'
-          buttonProps={{onClick: () => { openMailModal(id as string) }}}
-        />
-      </Stack>
+      renderCell: ({ id, row: { downloadLink } }) => (
+        <Stack justifyContent='start' direction='row' spacing={0.25}>
+          <ActionButton
+            title={'Descargar'}
+            icon='tabler:download'
+            buttonProps={{ href: downloadLink, download: true }}
+          />
+          <ActionButton
+            title='Eliminar'
+            icon='tabler:trash'
+            buttonProps={{ onClick: () => deleteFile(id as string) }}
+          />
+          <ActionButton
+            title='Enviar email'
+            icon='tabler:mail'
+            buttonProps={{
+              onClick: () => {
+                openMailModal(id as string)
+              }
+            }}
+          />
+        </Stack>
+      )
     }
   ]
 }
