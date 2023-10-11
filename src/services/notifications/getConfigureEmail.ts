f16ee525e0541fba96957a8d103e1c4a557f8011
gianicola <gianicolajara@gmail.com>
2023-10-11 16:03:34 -0400

    se agrega una transformacion de datos en el get

--- src/services/notifications/getConfigureEmail.ts
+++ src/services/notifications/getConfigureEmail.ts
@@ -1,9 +1,35 @@
+import { MailerSchema } from 'src/schemas'
 import { restRequestAuth } from '../rest-requests'
 
+type ResponseGetConfigureEmail = {
+  MAIL_ADDRESS?: string
+  MAIL_PORT?: string
+  MAIL_PROTOCOL?: string
+  MAIL_SERVER?: string
+  MAIL_USERNAME?: string
+}
+
+type ResponseGetConfigureEmailData = {
+  data?: ResponseGetConfigureEmail
+}
+
+const transformResponse = (data?: ResponseGetConfigureEmail) => {
+  if (!data) {
+    return MailerSchema.getDefault()
+  }
+
+  return {
+    email: data?.MAIL_ADDRESS || '',
+    smtpHost: data?.MAIL_SERVER || '',
+    smtpPort: data?.MAIL_PORT || '',
+    securityProtocol: data?.MAIL_PROTOCOL?.toUpperCase() || ''
+  }
+}
+
 const getConfigureEmail = async () => {
-  const response: Promise<any> = await restRequestAuth('GET', '/notify/settings')
-  console.log(response)
-  return response
+  const response: ResponseGetConfigureEmailData = await restRequestAuth('GET', '/notify/settings')
+
+  return transformResponse(response.data)
 }
 
 export default getConfigureEmail
