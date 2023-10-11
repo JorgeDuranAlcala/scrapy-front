9c0a1b6a95295f974b055108eab2fe4f8de7e1d1
gianicola <gianicolajara@gmail.com>
2023-10-11 17:10:57 -0400

    agregando invalidateQueries para que el estado siempre esta actualizado

--- src/hooks/useMail.tsx
+++ src/hooks/useMail.tsx
@@ -1,10 +1,21 @@
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'react-hot-toast'
 import { MailerData } from 'src/components'
 import getConfigureEmail from 'src/services/notifications/getConfigureEmail'
 import putConfigureEmail from 'src/services/notifications/putConfigureEmail'
 
 const useMail = () => {
+  const queryCache = useQueryClient()
+
+  const configureEmailQuery = useQuery({
+    queryKey: ['configure-mail-data'],
+    queryFn: async () => {
+      return await getConfigureEmail()
+    },
+    refetchOnMount: false,
+    refetchOnWindowFocus: false
+  })
+
   const mutateConfigureEmail = useMutation({
     mutationKey: ['configure-mail'],
     mutationFn: async (data: MailerData) => {
@@ -12,22 +23,13 @@ const useMail = () => {
     },
     onSuccess: () => {
       toast.success('Datos de configuración de Email actualizados')
+      queryCache.invalidateQueries({
+        queryKey: ['configure-mail-data']
+      })
     },
     onError: () => {
       toast.error('Algo fue mal al actualizar la configuración de Email')
     }
-  })
-
-  const configureEmailQuery = useQuery({
-    queryKey: ['configure-mail'],
-    queryFn: async () => {
-      return await getConfigureEmail()
-    },
-    refetchOnWindowFocus: false,
-    refetchOnMount: false,
-    refetchOnReconnect: false,
-
-    retry: 0
   })
 
   return {
